bc636d0e8a051ffe36daa8bfdd1fbfb70a4713e1
tejas <sadadetej.27@gmail.com>
2023-07-14 03:09:10 +0530

    Subscribing and Dispatching Actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,32 @@
 import classes from './Counter.module.css';
+import {  useSelector } from 'react-redux/es/hooks/useSelector';
+import { useDispatch } from 'react-redux';
 
 const Counter = () => {
+   //when we use seSelector react redux will automatically sent the subscripton of store to this component
+  const counter = useSelector(state => state.counter);
+  // the function e pass in useSelector that will executed by react redux which basically tell which data we wanna extract from store
+  // we pass function in it a function will recieve the state manage by redux AND return the part of the state which we wanna extract
+  const dispatch = useDispatch();
+
+  const IncrementHandler = () => {
+    dispatch({type : "increment"})
+  }
+
+  const DecrementHandler = () => {
+    dispatch({type : "decrement"})
+  }
+ 
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={IncrementHandler}>Increment</button>
+        <button onClick={DecrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
