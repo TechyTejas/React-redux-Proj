4797cadf11a455755b04630f4c35c093b037fcc0
tejas <sadadetej.27@gmail.com>
2023-07-16 08:40:20 +0530

    Auth Slice added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Counter from './components/Counter';
 import React, { Fragment } from 'react';
 import Header from './components/Header';
-import Auth from './components/Auth'
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile'
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 
 
 function App() {
+  const isAuth= useSelector(state => state.auth.isAuthenticated)
   return (
     <Fragment>
       <Header/>
-      <Auth/>
+     {!isAuth && <Auth/>}
+     {isAuth && <UserProfile/>}
       <Counter/> 
     </Fragment>
   );
--- src/Store/index.js
+++ src/Store/index.js
@@ -22,37 +22,6 @@ const counterSlice=createSlice({
     }
 })
 
-// const counterReducer=(state={counter:0, initialState},action)=>{
-//     if (action.type=== "increment"){
-//         return {
-//             counter: state.counter +1,
-//             showCounter:state.showCounter
-//         }
-//     }
-
-//     if (action.type=== "increase"){
-//         return {
-//             counter: state.counter +action.payload,
-//             showCounter:state.showCounter
-//         }
-//     }
-
-//     if (action.type=== "decrement"){
-//         return {
-//             counter: state.counter -1,
-//             showCounter:state.showCounter
-//         }
-//     }
-
-//     if(action.type==="toggle"){
-//         return{
-//             showCounter:!state.showCounter,
-//             counter:state.counter
-//         }
-//     }
-//     return state;
-// }
-
 const initialAuthState={
     isAuthenticated:false
 }
